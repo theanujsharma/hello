a89b291d8ea01070c667910b6ee80a8ef6a21981
Anuj Sharma <theanujsharma@outlook.com>
2024-10-27 00:20:14 +0530

    Update test_add.py

--- test_add.py
+++ test_add.py
@@ -1,7 +1,7 @@
 import unittest
 from add import add
 
-class TestAddFunction(unittest.TesCase):
+class TestAddFunction(unittest.TestCase):
   def test_add(self):
     self.assertEqual(add(2,3), 5)
     self.assertEqual(add(-1,1), 0)
